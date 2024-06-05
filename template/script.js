17b3a7b3d1330a0a584ce2dd98e578f362cc70e6
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-06-06 08:31:10 +0900

    ittannse-bu

--- template/script.js
+++ template/script.js
@@ -1,17 +1,23 @@
 //add article
-const article_text=document.body.innerHTML;
-document.body.innerHTML="";
-const article_box=document.createElement("div");
-article_box.id="article-content";
-article_box.innerHTML=article_text;
+const article_text = document.body.innerHTML;
+document.body.innerHTML = "";
+const article_box = document.createElement("div");
+article_box.id = "article-content";
+article_box.innerHTML = article_text;
 document.body.append(article_box);
 //add title
-const title_elem=document.querySelector("title");
-const article_title=document.createElement("h1");
-article_title.id="article-title";
-article_title.innerHTML=title_elem.innerHTML;
+const title_elem = document.querySelector("title");
+const article_title = document.createElement("h1");
+article_title.id = "article-title";
+article_title.innerHTML = title_elem.innerHTML;
 document.body.prepend(article_title);
 //add index
-const article_index=document.createElement("div");
-article_index.id="article-index";
+const article_index = document.createElement("div");
+article_index.id = "article-index";
+article_index.innerHTML = article_title.innerHTML;
 document.body.append(article_index);
+for (let i = 0; i < 10; ++i) {
+  const index_text = document.createElement("h1");
+  index_text.innerHTML = "123";
+  document.body.apeendChild(index_text);
+}
