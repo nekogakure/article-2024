b02460566542efc4adda69e637cd4a909fd2d0de
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-06-06 08:47:30 +0900

    とりあえず

--- template/script.js
+++ template/script.js
@@ -16,8 +16,16 @@ const article_index = document.createElement("div");
 article_index.id = "article-index";
 article_index.innerHTML = article_title.innerHTML;
 document.body.append(article_index);
-for (let i = 0; i < 10; ++i) {
-  const index_text = document.createElement("h1");
-  index_text.innerHTML = "123";
-  document.body.apeendChild(index_text);
+for (let i = 0; i < article_box.children.length; ++i) {
+  const article_box_child = article_box.children[i];
+  const index_text = document.createElement(article_box_child.tagName);
+  index_text.innerHTML = article_box_child.innerHTML;
+  const tags = { "H1": "H1", "H2": "H2", "H3": "H3", "H4": "H4" };
+  if (article_box_child.tagName in tags) {
+    document.querySelector("#article-index").append(index_text);
+  }
 }
+//add style
+// let The_Infinitys_Infinity = document.createElement("div");
+// The_Infinitys_Infinity.innerHTML = `<script defer src="/The-Infinitys-Infinity/The-Infinitys.js"></script><link rel="stylesheet" href="/The-Infinitys-Infinity/The-Infinitys.css" />`;
+// document.body.append(The_Infinitys_Infinity);
