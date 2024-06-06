3cc104cef82b34a5f2db1a2d89987318f4eef33a
The Infinitys <137460810+The-Infinitys@users.noreply.github.com>
2024-06-06 12:34:39 +0900

    ものはためし

--- template/script.js
+++ template/script.js
@@ -14,18 +14,23 @@ document.body.prepend(article_title);
 //add index
 const article_index = document.createElement("div");
 article_index.id = "article-index";
-article_index.innerHTML = article_title.innerHTML;
+article_index.innerHTML = "<h1>" + article_title.innerHTML + "</h1>";
 document.body.append(article_index);
 for (let i = 0; i < article_box.children.length; ++i) {
   const article_box_child = article_box.children[i];
-  const index_text = document.createElement(article_box_child.tagName);
-  index_text.innerHTML = article_box_child.innerHTML;
   const tags = { "H1": "H1", "H2": "H2", "H3": "H3", "H4": "H4" };
   if (article_box_child.tagName in tags) {
+    const index_text = document.createElement(article_box_child.tagName);
+    index_text.innerHTML = article_box_child.innerHTML;
     document.querySelector("#article-index").append(index_text);
   }
 }
 //add style
-// let The_Infinitys_Infinity = document.createElement("div");
-// The_Infinitys_Infinity.innerHTML = `<script defer src="/The-Infinitys-Infinity/The-Infinitys.js"></script><link rel="stylesheet" href="/The-Infinitys-Infinity/The-Infinitys.css" />`;
-// document.body.append(The_Infinitys_Infinity);
+let The_Infinitys_js = document.createElement("script");
+The_Infinitys_js.src = "/The-Infinitys-Infinity/The-Infinitys.js";
+document.body.append(The_Infinitys_js);
+fetch("/The-Infinitys-Infinity/The-Infinitys.css").then(res => res.text()).then(style => { 
+  const The_Infinitys_css =document.createElement("style");
+  The_Infinitys_css.innerHTML=style;
+  document.body.append(The_Infinitys_css);
+}).catch(err=>alert(err));
