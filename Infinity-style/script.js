820782efdeb0968fdbc10d515587cdd4dfffc68d
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-06-13 12:17:28 +0900

    Update script.js

--- Infinity-style/script.js
+++ Infinity-style/script.js
@@ -42,3 +42,13 @@ fetch("https://" + new URL(window.location.href).hostname + "/The-Infinitys-Infi
   The_Infinitys_js.innerHTML = style;
   document.body.append(The_Infinitys_js);
 }).catch(err => alert(err));
+//add code highlight.js
+document.body.append(`
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+
+<!-- and it's easy to individually load additional languages -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"></script>
+
+<script>hljs.highlightAll();</script>
+`);
