896583e1a30803cf9682cbb19e0251dc44cdb506
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-06-13 12:24:27 +0900

    Update script.js

--- Infinity-style/script.js
+++ Infinity-style/script.js
@@ -47,11 +47,11 @@ fetch("https://" + new URL(window.location.href).hostname + "/The-Infinitys-Infi
 const highlight = document.createElement("div");
 hightlight.innerHTML = `
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
 
 <!-- and it's easy to individually load additional languages -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/go.min.js"></script>
 
-<script>hljs.highlightAll();</script>
+<script>window.onload=()=>{hljs.highlightAll();};</script>
 `;
 document.body.append(highlight);
