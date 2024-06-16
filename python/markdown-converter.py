0a3e1c8d602f4bd851f3d994bf38eda70ac683ec
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-06-16 18:25:54 +0900

    SEOスコアを満点に

--- python/markdown-converter.py
+++ python/markdown-converter.py
@@ -7,7 +7,8 @@ article_temp_head = '''
 <html lang="Ja">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" /><meta name="description" content="The Infinity's awesome blog article" />
+    <meta name="description" content="The Infinity's awesome blog article" />
     <title>HERE TO THE TITLE</title>
     <link rel="stylesheet" href="../../Infinity-style/style.css" />
     <script defer src="../../Infinity-style/script.js"></script>
