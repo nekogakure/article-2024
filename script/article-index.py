f9c65d023c959392bef013275e0957d122491abd
The Infinity's <137460810+The-Infinitys@users.noreply.github.com>
2024-06-25 09:00:52 +0900

    修正

--- script/article-index.py
+++ script/article-index.py
@@ -32,7 +32,7 @@ def end():
   f=open(json_files_name,mode="w")
   def get_date(obj):
     return obj["date"]
-  result_obj["info"].sort(key=get_date)
+  result_obj["info"].sort(key=get_date, reverse=True)
   f.write(json.dumps(result_obj,indent=2))
   print(json.dumps(result_obj,indent=2))
   f.close()
